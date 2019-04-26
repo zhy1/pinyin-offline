68f4679e62c8573e645309aae8731bd53922ea00
zhangzhongcheng <zhangzhongcheng@ezbuy.com>
2019-04-26 11:15:42 +0800

    readme

--- pinyin_test.go
+++ pinyin_test.go
@@ -5,28 +5,28 @@ import (
 )
 
 func TestConvert(t *testing.T) {
-	str, err := New("张中诚").Split("").Mode(InitialsInCapitals).Convert()
+	str, err := New("我是中国人").Split("").Mode(InitialsInCapitals).Convert()
 	if err != nil {
 		t.Error(err)
 	}else{
 		t.Log(str)
 	}
 
-	str, err = New("张中诚").Split(" ").Mode(WithoutTone).Convert()
+	str, err = New("我是中国人").Split(" ").Mode(WithoutTone).Convert()
 	if err != nil {
 		t.Error(err)
 	}else{
 		t.Log(str)
 	}
 
-	str, err = New("张中诚").Split("-").Mode(Tone).Convert()
+	str, err = New("我是中国人").Split("-").Mode(Tone).Convert()
 	if err != nil {
 		t.Error(err)
 	}else{
 		t.Log(str)
 	}
 
-	str, err = New("张中诚").Convert()
+	str, err = New("我是中国人").Convert()
 	if err != nil {
 		t.Error(err)
 	}else{
